74f4e3b105e0c322d61326062d363f123c125d5e
josealeperdomo <perdomomolinajose@gmail.com>
2023-12-01 18:46:28 -0400

    ...

--- practica4.js
+++ practica4.js
@@ -5,12 +5,20 @@ let apiUrl = 'https://rickandmortyapi.com/api/character?page=1'
 let contador = 1
 
 const incremento = (num)=>{
-    if(contador > 1 && contador <= 42){
+    if(contador > 1 && contador < 42){
         contador += num
         let Url = 'https://rickandmortyapi.com/api/character?page=' + contador
         inicio(Url)
-    }else if(contador = 1 && num > 0 && contador <= 42){
+    }else if(contador == 1 && num > 0){
         contador += num
+        let Url = 'https://rickandmortyapi.com/api/character?page=' + contador
+        inicio(Url)
+    }else if(contador == 1 && num < 0){
+        contador = 42
+        let Url = 'https://rickandmortyapi.com/api/character?page=' + contador
+        inicio(Url)
+    }else if(contador == 42 && num > 0){
+        contador = 1
         let Url = 'https://rickandmortyapi.com/api/character?page=' + contador
         inicio(Url)
     }
@@ -35,7 +43,7 @@ const inicio = async (url)=>{
             imagen.src = personaje.image
             content.appendChild(parrafo);
             content.appendChild(imagen);
-
+            console.log(url);
     })
 }
 
